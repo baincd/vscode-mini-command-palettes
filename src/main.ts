cb9655f93c9150b808c1f5824f236f341db7b98c
Chris Bain <baincd@users.noreply.github.com>
2021-09-19 21:57:57 -0400

    Clean up extension code

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,29 @@ import * as vscode from 'vscode';
 import { MiniCommandPaletteItem, PaletteConfigs } from './types';
 
 export function activate(context: vscode.ExtensionContext) {
-	vscode.window.showErrorMessage("Activated!");
+	reload();
+}
 
-	const configs = vscode.workspace.getConfiguration("baincd.mini-command-palettes").get("paletteConfigs") as PaletteConfigs;
+function reload() {
+	const configs = getPaletteConfigs();
 
 	for (const paletteName in unproxy(configs)) {
-			vscode.commands.registerCommand("baincd.mini-command-palettes.cmds." + paletteName, () => showPalette(paletteName));
+		vscode.commands.registerCommand("baincd.mini-command-palettes.cmds." + paletteName, () => showPalette(paletteName));
 	}
-
 }
 
 function showPalette(paletteName: string) {
-	const configs = vscode.workspace.getConfiguration("baincd.mini-command-palettes").get("paletteConfigs") as PaletteConfigs;
+	const configs = getPaletteConfigs();
+
 	vscode.window.showQuickPick(configs[paletteName]).then( (selected) => {
 		if (selected) {
 			vscode.commands.executeCommand(selected.command, ...(selected.commandArgs || []));
 		}
 	});
+}
+
+function getPaletteConfigs() {
+	return vscode.workspace.getConfiguration("baincd.mini-command-palettes").get("paletteConfigs") as PaletteConfigs;
 }
 
 function unproxy(original: any): any {
